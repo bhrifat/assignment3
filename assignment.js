e11d2e69f0c997068c6241cfa4b511d63cae751e
Badrul Hasan Rifat <badrul.h.rifat>
2021-01-22 18:11:11 +0600

    Final Commit

--- assignment.js
+++ assignment.js
@@ -1,4 +1,9 @@
-//Conversion to Kilometer to meter
+// https://github.com/bhrifat/assignment3
+
+
+
+//Conversion of Kilometer to meter
+
 function kelometerToMeter(km) {
     //Checking the input is number or string
     if (typeof km !== "number") {
@@ -12,15 +17,14 @@ function kelometerToMeter(km) {
         return meter;
     }
 }
-console.log(kelometerToMeter(10));
-
-
+console.log(kelometerToMeter(97));
 
 
 
 
 
 //Budget Calculator
+
 function budgetCalculator(watch, phone, laptop) {
     //Checking the input is number or string
     if (typeof watch !== "number" || typeof phone !== "number" || typeof laptop !== "number") {
@@ -30,9 +34,9 @@ function budgetCalculator(watch, phone, laptop) {
     else if (watch < 0 || phone < 0 || laptop < 0) {
         return "Error: Input a positive number!";
     } else {
-        let watchPrice = 50 * watch;
-        let phonePrice = 100 * phone;
-        let laptopPrice = 500 * laptop;
+        let watchPrice = watch * 50;
+        let phonePrice = phone * 100;
+        let laptopPrice = laptop * 500;
         let totalPrice = watchPrice + phonePrice + laptopPrice;
         return totalPrice;
     }
@@ -41,32 +45,10 @@ let GetTotalPrice = budgetCalculator(7, 9, 3);
 console.log(GetTotalPrice);
 
 
-//Mega Friend
-var friendsName = ["Badrul", "Hasan", "Rifaaaaaaat", "Tahia", "Sagor", "Atik"];
-
-function megaFriend(friendsName) {
-    let longestName = friendsName[0];
-    // Checking Array is empty or not
-    if (friendsName.length === 0 || Array.isArray(friendsName) === false) {
-        return "Error: This is not an Array or empty Array";
-    }
-    //Checking Array Element is enough or not
-    else if (friendsName.length < 1) {
-        return "Error: You do not have enough element";
-    } else {
-        for (i = 0; i < friendsName.length; i++) {
-            if (friendsName[i].length > longestName.length) {
-                longestName = friendsName[i];
-            }
-        }
-        return longestName;
-    }
-}
-console.log(megaFriend(friendsName));
 
 
+// Hotel Cost
 
-// Hotel Cost Calculator
 function hotelCost(totalDay) {
     var totalCost = 0;
     // Checking the number is positive or negative
@@ -90,4 +72,32 @@ function hotelCost(totalDay) {
         return totalCost;
     }
 }
-console.log(hotelCost(21));
+console.log(hotelCost(51));
+
+
+//Mega Friend
+
+var friendsName = ["Badrul", "Hasan", "Rifaaaaaaat", "Tahia", "Sagor", "Atik"];
+
+function megaFriend(friendsName) {
+    let longestName = friendsName[0];
+    // Checking Array is empty or not
+    if (friendsName.length === 0 || Array.isArray(friendsName) === false) {
+        return "Error: This is not an Array or empty Array";
+    }
+    //Checking Array Element is enough or not
+    else if (friendsName.length < 1) {
+        return "Error: You do not have enough element";
+    } else {
+        for (i = 0; i < friendsName.length; i++) {
+            if (friendsName[i].length > longestName.length) {
+                longestName = friendsName[i];
+            }
+        }
+        return longestName;
+    }
+}
+console.log(megaFriend(friendsName));
+
+
+// End
